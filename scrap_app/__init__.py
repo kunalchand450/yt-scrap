452a3b28d8f1a15748f9b186fcfe41eaf2d98b44
kunal chand <81466590+kunalchand450@users.noreply.github.com>
2022-10-18 11:48:40 +0530

    refractor

--- scrap_app/__init__.py
+++ scrap_app/__init__.py
@@ -6,31 +6,31 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for, s
 import time
 BASE_DIR="/opt/disc_wt/"
 
-# def create_app(test_config=None):
-#     # create and configure the app
-#     app = Flask(__name__, instance_relative_config=True)
-#     app.config.from_mapping(
-#         SECRET_KEY='dev'
-#         # DATABASE=os.path.join(app.instance_path, 'scrap_app.sqlite'),   # Default configuration settings for flask app shoud be changed with frontend lib
-#     )
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev'
+        # DATABASE=os.path.join(app.instance_path, 'scrap_app.sqlite'),   # Default configuration settings for flask app shoud be changed with frontend lib
+    )
 #     if __name__ == "__main__":
 #         app.run()
-#     if test_config is None:
-#         # load the instance config, if it exists, when not testing
-#         app.config.from_pyfile('config.py', silent=True)
-#     else:
-#         # load the test config if passed in
-#         app.config.from_mapping(test_config)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-#     # ensure the instance folder exists
-#     try:
-#         os.makedirs(app.instance_path)
-#     except OSError:
-#         pass
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-app = Flask(__name__)
-if __name__ == "__main__":
-    app.run()
+# app = Flask(__name__)
+# if __name__ == "__main__":
+#     app.run()
 
 @app.route('/')
 def index():
